aa3c3f6496ae4aba5ab70899b4be051ef9f93326
Miguel Rojas <ma.rojas11@uniandes.edu.co>
2015-07-23 20:11:51 -0500

    Organizando

---
+++ Ejercicios/primer_script.sh
@@ -0,0 +1,5 @@
+#Este es mi primer script
+mkdir clase2/
+cd clase2/
+ls -a
+
---
+++ Ejercicios/seg_script.sh
@@ -0,0 +1,6 @@
+# Descargar libro metamorfosisi
+
+wget https://github.com/jngaravitoc/HerramientasComputacionales/raw/master/Lectures/02.Unix-TextEditors/Hands-on/metamorphosis.txt
+wc -l metamorphosis.txt
+head -10 metamorphosis.txt
+tail -20 metamorphosis.txt
